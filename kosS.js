43186f9a6388c8d78163bbb2a7b05a19331fea1f
conanskyforce <conanskyforce@163.com>
2019-11-10 14:30:50 +0800

    correct error

--- kosS.js
+++ kosS.js
@@ -6,7 +6,27 @@ const request = require('request-promise-native')
 const urlBase = 'http://localhost:9200/'
 const PORT = 9525
 const app = new Koa()
+let corsWhiteList = []
 const router = new Router()
+app.use(async (ctx, next) => {
+  let { referer } = ctx.request.headers
+  console.log(referer)
+  referer[referer.length-1] == '/'?(referer = referer.slice(0,-1)):null
+  if(corsWhiteList.length){
+    corsWhiteList.forEach(white=>{
+      ctx.set("Access-Control-Allow-Origin", white); 
+    })
+  }
+  if(~referer.indexOf('devdapps.top')){
+    ctx.set("Access-Control-Allow-Origin", referer);
+  }
+  ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
+  ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type");
+  ctx.set("Content-Type", "application/json;charset=utf-8");
+  ctx.set("Access-Control-Allow-Credentials", true);
+  ctx.set("Access-Control-Max-Age", 300);
+  await next();
+})
 router.get('/', async (ctx,next) =>{
   ctx.body = await request(urlBase+'_search')
 })
