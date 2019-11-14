3142d8f04d4360dc7deeb0c0c56afe99d89657c0
conanskyforce <conanskyforce@163.com>
2019-11-14 09:42:03 +0800

    updateConfigServer

--- kosS.js
+++ kosS.js
@@ -1,23 +1,30 @@
 
 const Koa = require('koa')
 const Router = require('koa-router')
+const static = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const request = require('request-promise-native')
 const urlBase = 'http://localhost:9200/'
-const PORT = 9525
+const renderView = require('./views/index').renderView
+const PORT = 9540
 const app = new Koa()
-let corsWhiteList = []
+let corsWhiteList = [
+  'http://106.54.64.20:9540'
+]
 const router = new Router()
+app.use(static('.'))
+app
+.use(bodyParser())
 app.use(async (ctx, next) => {
   let { referer } = ctx.request.headers
   console.log(referer)
-  referer[referer.length-1] == '/'?(referer = referer.slice(0,-1)):null
+  referer && referer[referer.length-1] == '/'?(referer = referer.slice(0,-1)):null
   if(corsWhiteList.length){
     corsWhiteList.forEach(white=>{
       ctx.set("Access-Control-Allow-Origin", white); 
     })
   }
-  if(~referer.indexOf('devdapps.top')){
+  if(referer && ~referer.indexOf('devdapps.top')){
     ctx.set("Access-Control-Allow-Origin", referer);
   }
   ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
@@ -27,9 +34,12 @@ app.use(async (ctx, next) => {
   ctx.set("Access-Control-Max-Age", 300);
   await next();
 })
+
 router.get('/', async (ctx,next) =>{
-  ctx.body = await request(urlBase+'_search')
+  ctx.type = 'text/html; charset=utf-8';
+  ctx.body = await renderView('home')
 })
+
 router.post('/query', async (ctx,next) =>{
   let body = ctx.request.body;
   console.log(body)
@@ -37,18 +47,27 @@ router.post('/query', async (ctx,next) =>{
     ctx.status = 404
     ctx.body = '{query:{match:{x:x}}} format needed'
   }
-  ctx.body = await request({
-    url: `${urlBase}_search`,
-    body: JSON.stringify(body),
-    headers:{
-      'Content-Type':'application/json'
-    }
-  })
+  let res;
+  try{
+    res = await request({
+      url: `${urlBase}_search`,
+      body: JSON.stringify(body),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+  }catch(err){
+    console.log(`await request error:`)
+    console.log(err)
+    res = 'Internal Error'
+  }
+  ctx.body = res
 })
+
 app
-.use(bodyParser())
 .use(router.allowedMethods())
 .use(router.routes())
 .listen(PORT,()=>{
   console.log(`server running at: http://localhost:${PORT}`)
 })
+
