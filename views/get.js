ee125af3ac9cce9d4ac6575f2396bc8792bc13fd
conanskyforce <conanskyforce@163.com>
2019-11-15 10:07:37 +0800

    update kosElasticServer logic

--- views/get.js
+++ views/get.js
@@ -2,7 +2,7 @@ const form = document.forms[0]
 const input = document.getElementsByClassName('input')[0]
 const submit = document.querySelector('[type=submit]')
 
-let res = document.querySelector('.res')
+let result = document.querySelector('.res')
 
 submit.addEventListener('click',(e)=>{
   e.preventDefault();
@@ -14,10 +14,44 @@ submit.addEventListener('click',(e)=>{
     headers:{'Content-Type':'application/json'},
     data:{query:{match:{name:value,}},from:0,size:10}
     })
-  .then(res=>res.json())
   .then(appendToRes)
+  .catch(err=>{
+    console.error(err)
+  })
 })
 
 function appendToRes(res){
-  res.innerHTML = res.join('\n')
+  if(res.data) res = res.data
+  let took = res.took;
+  let hits = res.hits.hits;
+  result.innerHTML = `<h2>耗时：${took}ms</h2>`+`<div class="list-container">
+  ${generateItem(hits)}
+  </div>`
 }
+function generateItem(hits){
+  let dataList = hits.map(hit=>hit._source);
+  let ret = ''
+  dataList.forEach(data=>{
+    let {
+      address,
+      businessScope,
+      capital,
+      character,
+      city,
+      code,
+      legalRepresentative,
+      name,
+      province,
+      registrationDay,
+    } = data;
+    ret +=`
+    <div class="list-item">
+    <p>address : ${address},businessScope : ${businessScope},capital : ${capital},character : ${character},city : ${city}</p>
+    <p>code : ${code},legalRepresentative : ${legalRepresentative},name : ${name},province : ${province},registrationDay : ${registrationDay}</p>
+    <hr/>
+    </div>
+    `
+  })
+  return ret
+}
+
